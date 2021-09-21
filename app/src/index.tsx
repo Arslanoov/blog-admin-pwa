a956736e0aa9e08587a55db1eecc0798ee97c1fd
Arslanoov <rasularslanoov@gmail.com>
2021-09-21 21:48:30 +0500

    feat(app): Added routing

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom"
 import reportWebVitals from "./bootstrap/reportWebVitals"
 import * as serviceWorkerRegistration from "./bootstrap/registerServiceWorker"
 
-import App from "./ui/components/App"
+import App from "./ui"
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ app/src/ui/pages/Auth/Login/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Button from "@mui/material/Button"
+
+const Login: React.FC = () => {
+  return (
+    <Button>
+      Login
+    </Button>
+  )
+}
+
+export default Login
